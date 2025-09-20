ba0085bd1f858f7322999366645403869101251b
DiarBerisha <154000780+DiarBerisha@users.noreply.github.com>
2025-09-20 12:48:59 +0200

    1
    1

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?php bloginfo('charset')?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digital School</title>
+    <?php wp_head(); ?>
+</head>
+<body id="top" <?php body_class();?>>
+
+<header class="site-header">
+    <a class="site-title" href="<?php echo esc_url(home_url('/'))?>">
+    <?php bloginfo('name')?>
+</a>
+
+<?php if(get_bloginfo('description')): ?>
+    <p class="site-tagline"><?php bloginfo('description') ?></p>
+<?php endif; ?>
+
+
+</header>
+
+    
---
+++ index.php
@@ -0,0 +1,4 @@
+
+
+<main>Digital School</main>
+
