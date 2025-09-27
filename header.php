eb29e69590d1a912f09364ad8e97a81291120169
DiarBerisha <154000780+DiarBerisha@users.noreply.github.com>
2025-09-27 12:49:22 +0200

    f

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
     <meta charset="<?php bloginfo('charset')?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Digital School</title>
+    
+
     <?php wp_head(); ?>
 </head>
 <body id="top" <?php body_class();?>>
