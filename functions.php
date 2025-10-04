a4168fee7ce3980fc667dbd9fbee0dbbac7e098e
DiarBerisha <154000780+DiarBerisha@users.noreply.github.com>
2025-10-04 12:49:41 +0200

    b

--- functions.php
+++ functions.php
@@ -4,5 +4,11 @@ function ds_style(){
 }
 add_action('wp_enqueue_scripts', 'ds_style');
 
+function ds_menu(){
+    register_nav_menus(array(
+        'primary'=>'Primary Menu'
+    ));
+}
 
+add_action('after_setup_theme', 'ds_menu');
 ?>
