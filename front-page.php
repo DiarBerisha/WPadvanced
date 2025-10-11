e38394b7cff05957d5302b1a265c089a36235057
DiarBerisha <154000780+DiarBerisha@users.noreply.github.com>
2025-10-11 12:49:16 +0200

    d

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main>
+    <section class="hero">
+        <img class="hero-banner"
+        src="<?php echo esc_url(get_theme_file_uri('images/img.png')); ?>" alt="Alt Banner">
+    </section >
+    <section class="features">
+    <h2 id="heading">Why Choose Us?</h2>
+    <li>
+        <img src="" alt="">
+    </li>
+    </section>
+</main>
+
+<?php get_footer(); ?>
